b7bc4e0d819ceb157b4cf4f6b8a26ce6258837b9
İrem Çelebi <iremcelebi2404@gmail.com>
2023-02-19 23:36:52 +0300

    Bitmiş hali. Alıştırma 2 tamamlandı

--- index.js
+++ index.js
@@ -40,17 +40,19 @@ Aşağıdakileri yapmak için aşağıdaki kopyalama işlevini kullanın:
 */
 
 console.clear();
-function kopyala(kopyanacakDizi){
+function kopyala(kopyanacakDizi) {
   return [...kopyanacakDizi];
-
 }
-console.log("başlangıç");
+console.log(
+  "-------------------------------------------------------------------"
+);
+console.log("Görev 1: Diziyi kopyalama!");
 console.log(kopyala(orijinalTatlar));
 console.log(orijinalTatlar.length);
 
-  //şöyle de olur:
-   //let c=  [...kopyanacakDizi];
-   //return c;
+//şöyle de olur:
+//let c=  [...kopyanacakDizi];
+//return c;
 
 //şunu yapamıyoruz:
 //return kopyalanacakdizi=orijinaltatlar
@@ -68,15 +70,17 @@ Bir dizinin tam olarak 25 çeşit olduğunu onaylayın. İşleviniz şunları ka
 Örneğin: dizi25Cesit(orijinalTatlar) kodunuz düzgün çalışıyorsa true değerini döndürür.
 */
 
-
-function dizi25Cesitmi(array){
- if (array.length == 25 ){
-  return true
- }
- else{
-  return false
- }
+function dizi25Cesitmi(array) {
+  if (array.length == 25) {
+    return true;
+  } else {
+    return false;
+  }
 }
+console.log(
+  "-------------------------------------------------------------------"
+);
+console.log("Görev 2: dizi25Cesit");
 console.log(dizi25Cesitmi(orijinalTatlar));
 
 //2 eşittile 3 eşittir arasındaki fark 3 eşittirde tipinin de aynı olması bekleniyor
@@ -93,16 +97,18 @@ Aşağıdakileri yapmak için cesitEkle işlevini kullanın:
   Örneğin: cesitEkle(orijinalTatlar, "Kakule") işlevi doğru çalıştığında ["Kakule", "Muz",..."Vanilya"] şeklinde dönmelidir
 */
 
-
-function cesitEkle(cesitEklenecekDizi, eklenecekTat){
-cesitEklenecekDizi.unshift (eklenecekTat);
-return cesitEklenecekDizi;
-//eğer return kısmına cesitEklenecekDizi.unshift (eklenecekTat) yazarsam 26 değerini döndürüyor fonksiyon
- //push diyince sona unshift diyince başa ekliyor
+function cesitEkle(cesitEklenecekDizi, eklenecekTat) {
+  cesitEklenecekDizi.unshift(eklenecekTat);
+  return cesitEklenecekDizi;
+  //eğer return kısmına cesitEklenecekDizi.unshift (eklenecekTat) yazarsam 26 değerini döndürüyor fonksiyon
+  //push diyince sona unshift diyince başa ekliyor
 }
-
+console.log(
+  "-------------------------------------------------------------------"
+);
+console.log("Görev 3: cesitEkle");
 console.log(cesitEkle(orijinalTatlar, "Kakule"));
-console.log("üçüncü görev bitti artık " + orijinalTatlar.length +" çeşit var");
+console.log("üçüncü görev bitti artık " + orijinalTatlar.length + " çeşit var");
 //artık orijinal hali buna dönüştü
 
 /* Cörev 4:
@@ -117,39 +123,36 @@ Aşağıdakileri yapmak için sonCesitiKaldir işlevini kullanın:
    Örneğin: sonCesitiKaldir(orijinalTatlar) çalıştırıldığında ["Kakule", "Muz",..."Çilek"] döndürülür.
 */
 
+function sonCesitiKaldir(cesitKaldirilacakDizi) {
+  cesitKaldirilacakDizi.pop();
+  return cesitKaldirilacakDizi;
 
-function sonCesitiKaldir(cesitKaldirilacakDizi){
-
-   cesitKaldirilacakDizi.pop();
-   return cesitKaldirilacakDizi;
-
-// pop metodu sondaki elemanı kaldırır.   
-//  return cesitKaldirilacakDizi.pop(); diyince vanilyayı basıyor çünkü pop metodu çıkarılmış elemanı döndürür
-//biz diziyi bassın istiyoruz
-//metodun return ettiği değer bizim için önemli bu yüzden
+  // pop metodu sondaki elemanı kaldırır.
+  //  return cesitKaldirilacakDizi.pop(); diyince vanilyayı basıyor çünkü pop metodu çıkarılmış elemanı döndürür
+  //biz diziyi bassın istiyoruz
+  //metodun return ettiği değer bizim için önemli bu yüzden
 }
 
-//console.log(orijinalTatlar);
-//console.log("deneme");
-
+console.log(
+  "-------------------------------------------------------------------"
+);
+console.log("Görev 4: sonCesidiKaldir");
 sonCesitiKaldir(orijinalTatlar);
 console.log(orijinalTatlar);
-console.log("dördüncü görev bitti tekrar " + orijinalTatlar.length +" çeşit var");
+console.log(
+  "dördüncü görev bitti tekrar " + orijinalTatlar.length + " çeşit var"
+);
 
 //console.log(orijinalTatlar);
 //console.log(sonCesitiKaldir(orijinalTatlar));
 //console.log("deneme");
 //console.log(orijinalTatlar);
 
-
-
-
 //Pop() dizinin son öğesini kaldırır. ve orijinal arrayi değiştirir
 // length değerini bir azaltarak da sondaki ögeyi silebilirsin
 //shift() dizinin ilk öğesini kaldırır
 //Splice(x, y)  İlk argüman, öğeleri eklemeye veya kaldırmaya başlayacağınız yeri belirler. İkinci argüman kaldırılacak öğe sayısını belirtir.
 //Üçüncü ve sonraki argümanlar isteğe bağlıdır; diziye eklenecek öğeleri belirtirler.
-
 
 /* Görev 5:
 Dizideki belirli bir indeksteki çeşniyi döndüren bir işlev yazın.
@@ -162,11 +165,20 @@ Aşağıdakileri yapmak için aşağıdaki indekstekiCesitiGetir işlevini kulla
    Örneğin: indekstekiCesitiGetir(orijinalTatlar, 2) çalıştırılmasıyla, Kakule'in başarıyla eklendiği varsayarsak sonuç "Ceviz" olucaktır.
 */
 
-function indekstekiCesitiGetir(dizi, istenenEleman){
-return dizi[istenenEleman]
+function indekstekiCesitiGetir(dizi, istenenEleman) {
+  return dizi[istenenEleman];
 }
-console.log (indekstekiCesitiGetir(orijinalTatlar, 2));
-console.log("beşinci görev bitti " + indekstekiCesitiGetir(orijinalTatlar, 2)+ " çeşidini yazdırdık");
+
+console.log(
+  "-------------------------------------------------------------------"
+);
+console.log("Görev 5: indekstekiCesitiGetir");
+console.log(indekstekiCesitiGetir(orijinalTatlar, 2));
+console.log(
+  "beşinci görev bitti " +
+    indekstekiCesitiGetir(orijinalTatlar, 2) +
+    " çeşidini yazdırdık"
+);
 //yine 1. eleman 0 ile gösterilir unutma
 
 /* Görev 6:
@@ -189,17 +201,17 @@ Aşağıdakileri yapmak için ismeGoreCesitCikar işlevini kullanın:
 //y kaç eleman çıkarmak istediğini gösterir.
 //eğer x konumuna yeni elemanlar eklemek istiyorsan "elma", "armut" diyip ekleyebilirsin.
 
-
-function ismeGoreCesitCikar(dizi, tat){
-
+function ismeGoreCesitCikar(dizi, tat) {
   let indeks = dizi.indexOf(tat);
   // tat kısmına biz string bir değer giriyoruz bunu sayıya dönüştürdük indeksof la
-    dizi.splice(indeks, 1);
-  return dizi
+  dizi.splice(indeks, 1);
+  return dizi;
 }
-
+console.log(
+  "-------------------------------------------------------------------"
+);
+console.log("Görev 6: ismeGoreCesitCikar");
 console.log(ismeGoreCesitCikar(orijinalTatlar, "Tarçın"));
-
 
 /* Görev 7:
 
@@ -221,35 +233,30 @@ Aşağıdakileri yapmak için ismeGoreFiltrele işlevini kullanın:
   Bu sorunu çözmek için GELİŞMİŞ DİZİ YÖNTEMLERİNİ (yani .filter) KULLANMAYIN.
 */
 
-
-
-console.log("7.görev");
-
 //console.log(orijinalTatlar.includes("Çikolata"));  ---> false döner string değerlerde tam eşleşme arıyor
 //console.log(orijinalTatlar.includes("Çikolata", 6));  ---> aynı şekilde false döner. yukarıda tüm dizide arıyor, yanına sayı ekleyerek spesifik bir noktada arayabilirsin
 //console.log(orijinalTatlar[6].includes("Çikolata")); --->true döner
 
-
-function ismeGoreFiltrele(dizi, tat){
-  let yenidizi=[]; 
-//diziyi for döngüsünün içine koyarsan her çalıştığında dizi sıfırlanır. ki zaten return bulamayacağı için hata verecek
-for(let i=0; i< dizi.length; i++){
-
-  if (dizi[i].includes(tat)==true){ 
-    //true yerine 1 desen de çalışıyor. ama ===1 dersen çalışmıyor.
-    //hiçbir şey demezsen de çalışıyor çünkü zaten doğruysa ifin içine giriyor
-
-    console.log("if çalışıyor");
-    yenidizi.push(dizi[i]);
+function ismeGoreFiltrele(dizi, tat) {
+  let yenidizi = [];
+  //diziyi for döngüsünün içine koyarsan her çalıştığında dizi sıfırlanır. ki zaten return bulamayacağı için hata verecek
+  for (let i = 0; i < dizi.length; i++) {
+    if (dizi[i].includes(tat) == true) {
+      //true yerine 1 desen de çalışıyor. ama ===1 dersen çalışmıyor.
+      //hiçbir şey demezsen de çalışıyor çünkü zaten doğruysa ifin içine giriyor
+      yenidizi.push(dizi[i]);
 
       // yenidizi.push(dizi(i)); bu hata veriyor
-} 
+    }
+  }
+  return yenidizi;
 }
-return yenidizi
-}
-console.log(ismeGoreFiltrele(orijinalTatlar, "Çikolata"));
+console.log(
+  "-------------------------------------------------------------------"
+);
+console.log("Görev 7: ismeGoreFiltrele");
 
-// if (dizi.includes(tat, i)==true){
+console.log(ismeGoreFiltrele(orijinalTatlar, "Çikolata"));
 
 /* ALIŞTIRMA */
 
@@ -263,30 +270,33 @@ Aşağıdakileri yapmak için ortalamaKelimeSayisi işlevini kullanın:
    Örneğin: ortalamaKelimeSayisi(orijinalTatlar) 0 ile 2 arasında bir sayı döndürmelidir.
 */
 
-function ortalamaKelimeSayisi(dizi){
-let toplamKelime=0;
-for(let i=0; i<dizi.length; i++){
-  toplamKelime++;
-if (dizi[i].includes(" "))
-toplamKelime++;
-}
+function ortalamaKelimeSayisi(dizi) {
+  let toplamKelime = 0;
+  for (let i = 0; i < dizi.length; i++) {
+    toplamKelime++;
+    if (dizi[i].includes(" ")) toplamKelime++;
+  }
 
-return toplamKelime/dizi.length
+  return toplamKelime / dizi.length;
 }
+console.log(
+  "-------------------------------------------------------------------"
+);
+console.log("Alıştırma 1: ortalamaKelimeSayisi");
 console.log(ortalamaKelimeSayisi(orijinalTatlar));
 
-function dahaDogrusu (dizi){
-// let kelimeler = dizi.split(" "); bu şekilde yazamıyoruz çünkü diziyi split edemezsin
+function dahaDogrusu(dizi) {
+  // let kelimeler = dizi.split(" "); bu şekilde yazamıyoruz çünkü diziyi split edemezsin
 
-let toplamKelime=0;
-for(let i=0; i<dizi.length; i++){
-let kelimeler = dizi[i].split(" ");
-toplamKelime +=kelimeler.length;
-}
-return toplamKelime/dizi.length;
+  let toplamKelime = 0;
+  for (let i = 0; i < dizi.length; i++) {
+    let kelimeler = dizi[i].split(" ");
+    toplamKelime += kelimeler.length;
+  }
+  return toplamKelime / dizi.length;
 }
 
-console.log (dahaDogrusu(orijinalTatlar));
+console.log(dahaDogrusu(orijinalTatlar));
 
 /* ALIŞTIRMA 2:
 Firma mevcut tatların yanında artık mevsimlik lezzetler ve hatta bölgesel lezzetler de sunmaktadır. Toplam 25 lezzet aromasını
@@ -300,54 +310,67 @@ Aşağıdakileri yapmak için rastgeleTatlar işlevini ve yeni dizileri kullanı
   Örneğin: rastgeleTatlar(orijinalTatlar, yeniTatlar, mevsimlikTatlar, bolgeselTatlar) çalıştırıldığında ["Kestane", "Ballı Badem,"..."Hindistan Cevizi", "Kuru üzüm"].
 */
 
+function rastgeleTatlar(arr1, arr2, arr3, arr4) {
+  const yeniArr = [...arr1, ...arr2, ...arr3, ...arr4];
+  let bosArr = [];
+  for (let i = 0; i <= 25; i++) {
+    let sayi = Math.floor(Math.random() * yeniArr.length);
+    bosArr.push[sayi];
+    yeniArr.splice(sayi, 1);
+    console.log(sayi, yeniArr[sayi]);
+  }
 
-function rastgeleTatlar(/*kod buraya*/){
-  /*kod buraya*/
+  return yeniArr;
 }
 
-// NEW DATA ARRAYS FOR STRETCH 2 ⬇️
-// const yeniTatlar = [
-//   "Badem",
-//   "Ballı Badem",
-//   "Fıstık Ezmesi",
-//   "Profiterol",
-//   "Madlen Çikolata"
-// ]
+const yeniTatlar = [
+  "Badem",
+  "Ballı Badem",
+  "Fıstık Ezmesi",
+  "Profiterol",
+  "Madlen Çikolata",
+];
 
-// const mevsimlikTatlar = [
-// "Pekan",
-// "Kaju",
-// "Çikolatalı Mousse",
-// "Fransız Vanilyası",
-// "Yumurta",
-// "Alman çikolatası",
-// "Kek üzerine krema",
-// "Hindistan Cevizi",
-// "Kaymaklı Biskuvi",
-// "Beyaz Çikolata",
-// "Mango"
-// ]
+const mevsimlikTatlar = [
+  "Pekan",
+  "Kaju",
+  "Çikolatalı Mousse",
+  "Fransız Vanilyası",
+  "Yumurta",
+  "Alman çikolatası",
+  "Kek üzerine krema",
+  "Hindistan Cevizi",
+  "Kaymaklı Biskuvi",
+  "Beyaz Çikolata",
+  "Mango",
+];
 
-// const bolgeselTatlar = [
-// "Kaymak",
-// "Karpuz",
-// "Karadut",
-// "Turunç",
-// "Portakal",
-// "Yogurt",
-// "Krem Peynir",
-// "Kakao",
-// "Karamel macchiato",
-// "Kuru üzüm",
-// "Peynir",
-// "Karamel"
-// ]
-
+const bolgeselTatlar = [
+  "Kaymak",
+  "Karpuz",
+  "Karadut",
+  "Turunç",
+  "Portakal",
+  "Yogurt",
+  "Krem Peynir",
+  "Kakao",
+  "Karamel macchiato",
+  "Kuru üzüm",
+  "Peynir",
+  "Karamel",
+];
+console.log(
+  "-------------------------------------------------------------------"
+);
+console.log("Alıştırma 2: rastgeleTatlar");
+console.log(
+  rastgeleTatlar(orijinalTatlar, yeniTatlar, mevsimlikTatlar, bolgeselTatlar)
+);
 
 /* Lütfen bu satırın altındaki hiçbir şeyi değiştirmeyin */
-function sa(){
-  console.log('Calışıyor');
-  return 'as';
+function sa() {
+  console.log("Calışıyor");
+  return "as";
 }
 sa();
 module.exports = {
@@ -360,6 +383,5 @@ module.exports = {
   kopyala,
   ismeGoreFiltrele,
   ortalamaKelimeSayisi,
-  rastgeleTatlar
-}
-
+  rastgeleTatlar,
+};
